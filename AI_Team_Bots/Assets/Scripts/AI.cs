1c7ab56ca98abfb643376868f96f3ffdcfcbfa1e
Retrophear <cd33my@gmail.com>
2016-03-10 18:11:08 +0000

    Ver. 1.0
    First ver

---
+++ AI_Team_Bots/Assets/Scripts/AI.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AI : MonoBehaviour
+{
+
+    public Transform goal;
+    public List<GameObject> friendlyTeam;
+
+    //Memory//
+    public Vector3 teamLKP;
+
+
+    void Awake()
+    {
+        GetTeam();
+    }
+    void Start()
+    {
+        //NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        //agent.destination = goal.position;
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(CoRoMethod(CommunicatePosition,2f));
+    }
+    void FixedUpdate()
+    {
+
+    }
+    private void GetTeam()
+    {
+        friendlyTeam = new List<GameObject>();
+        var t = GameObject.FindGameObjectsWithTag(gameObject.tag.ToString());
+
+        foreach (GameObject gob in t)
+        {
+            friendlyTeam.Add(gob);
+        }
+    }
+
+    private void CommunicatePosition()
+    {
+        foreach (GameObject ob in friendlyTeam)
+        {
+            if (ob != gameObject)
+            {
+                ob.GetComponent<AI>().teamLKP = gameObject.transform.position;
+                Debug.Log("Fired from " + gameObject.name + " " + Time.realtimeSinceStartup);
+            }
+        }
+
+    }
+
+   IEnumerator CoRoMethod(Action Method,float waitTime)
+    {
+
+        yield return new WaitForSeconds(waitTime);
+        Method();
+    }
+
+
+}
